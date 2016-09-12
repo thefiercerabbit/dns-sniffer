3faa25531aaddba227eea448f55ab776d387408d
Clément Dallard <dallard@lirmm.fr>
2016-09-13 00:42:12 +0200

    better better, who knows

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-MAC_OUT="mac_addr_out_interface"
-MAC_IN="mac_addr_local_interface"
-PSK="0d096bac38bc72b10dbb91b9735f39f84b5a991d0d63fefe11f9e05d6dcbc341"
-CAPTURE_PROGR='tshark'
-CAPTURE_FLAGS='-l -o wlan.enable_decryption:TRUE -o "uat:80211_keys:\"wpa-psk\",\"$PSK\"" -Y "(dns.flags.response==0 and dns.qry.type==1) or (bootp.option.type==53)" -Tfields -E "separator=|" -e "wlan.sa" -e "ip.src" -e "ip.dst" -e "bootp.id" -e "dns.qry.name" -e "frame.time_epoch" -e "_ws.col.Protocol" -q -i wlan0 -f "(link[0] != 0x80) and ((wlan ra $MAC_IN) or (wlan ta $MAC_IN) or (wlan ra MAC_OUT) or (wlan ta $MAC_OUT))"'
+MAC_OUT="internet_world_mac_addr"
+MAC_IN="local_network_mac_addr"
+BSSID="your_bssid"
+PASSWORD="your_wifi_password"
+CAPTURE_PROGR='dumpcap'
+CAPTURE_FLAGS="-q -i wlan0 -I -f '(link[0] != 0x80) and ((wlan ra $MAC_IN) or (wlan ta $MAC_IN) or (wlan ra $MAC_OUT) or (wlan ta $MAC_OUT))' -w -"
+FILTER_PROGR='tshark'
+FILTER_FLAGS="-l -o wlan.enable_decryption:TRUE -o 'uat:80211_keys:\"wpa-pwd\",\"$PASSWORD:$BSSID\"' -r - -Y '(dns.flags.response==0 and dns.qry.type==1) or (bootp.option.type==53)' -Tfields -E separator='|' -e wlan.sa -e ip.src -e ip.dst -e bootp.id -e dns.qry.name -e frame.time_epoch -e _ws.col.Protocol"
 
 if [ `whoami` != "root" ]
 then
@@ -17,7 +20,7 @@ then
 fi
 
 
-eval "$CAPTURE_PROGR $CAPTURE_FLAGS" |
+eval "$CAPTURE_PROGR $CAPTURE_FLAGS" | eval "$FILTER_PROGR $FILTER_FLAGS" |
 while IFS='|' read MAC_ADDR IP_SRC IP_DST BOOTP_ID URL TIMESTAMP PROTOCOL; do
     if [[ -n "$BOOTP_ID" ]]; then
 	echo "INSERT INTO LOG VALUES ('$MAC_ADDR','$IP_SRC','$IP_DST','$BOOTP_ID','$TIMESTAMP','$PROTOCOL');"
