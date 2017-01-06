3058f3c8b749beffce37294f1d4dd6a9cb788e7a
C <thefiercerabbit@gmail.com>
2017-01-06 16:55:56 +0000

    dhcp/dns in two tables + dhcp requested ip

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -1,27 +1,21 @@
-
 function _exit_function() {
     kill -9 0
 }
 
 trap "_exit_function" 0
 
-INTERFACE="wlan0"
-CHANNEL="11"
-OUT_FILE="/home/$USER/dns.db"
+# Your interface should already be in monitor mode,
+# otherwise add -I option to the capture flags.
+# One could also change the mode before setting the channel (see below).
+INTERFACE="wlan0mon" 
+OUT_FILE="/tmp/dns.db" # Change if you want your file to be saved after reboot.
+
+SSID="YourBox"
+PASSWORD="YourPassword"
+CHANNEL="3"
+RTR_MAC="01:23:45:67:89:AB"
+
 OPTIONS="w:i:"
-RTR_MAC="XX:XX:XX:XX:XX:XX"
-PASSWORD="password1234"
-CAPTURE_PROGR='dumpcap'
-CAPTURE_FLAGS="-q -i $INTERFACE -I -f '(link[0] != 0x80) and (wlan ra $RTR_MAC or wlan ta $RTR_MAC or wlan host $RTR_MAC)' -w -"
-FILTER_PROGR='tshark'
-FILTER_FLAGS="-l -o wlan.enable_decryption:TRUE -o 'uat:80211_keys:\"wep\",\"$PASSWORD\"' -r - -Y '(dns.flags.response==0 and dns.qry.type==1) or (bootp.option.type==53)' -Tfields -E separator='|' -e wlan.sa -e ip.src -e ip.dst -e bootp.id -e dns.qry.name -e frame.time_epoch -e _ws.col.Protocol"
-
-if [ `whoami` != "root" ]
-then
-    echo "This script needs root privileges"
-    exit 1
-fi
-
 while getopts $OPTIONS opt; do
     case $opt in
 	'w' ) OUT_FILE="$OPTARG" ;;
@@ -30,25 +24,38 @@ while getopts $OPTIONS opt; do
     esac
 done
 
-ifconfig $INTERFACE down
-iwconfig $INTERFACE mode monitor
-ifconfig $INTERFACE up
+CAPTURE_PROGR='dumpcap'
+CAPTURE_FLAGS="-q -i $INTERFACE -f '(link[0] != 0x80) and (wlan ra $RTR_MAC or wlan ta $RTR_MAC or wlan host $RTR_MAC)' -w -"
+FILTER_PROGR='tshark'
+FILTER_FLAGS="-l -o wlan.enable_decryption:TRUE -o 'uat:80211_keys:\"wpa-pwd\",\"${PASSWORD}:${SSID}\"' -r - -Y '(dns) or (bootp.option.type==53)' -Tfields -E separator='|' -e frame.time_epoch -e wlan.ta -e wlan.ra -e ip.src -e ip.dst -e bootp.id -e bootp.option.requested_ip_address -e dns.qry.name -e _ws.col.Protocol -e dns.flags.response -e dns.qry.class -e dns.qry.type "
+
+if [ `whoami` != "root" ]
+then
+    echo "This script needs root privileges"
+    exit 1
+fi
+
+# ifconfig $INTERFACE down
+# iwconfig $INTERFACE mode monitor
+# ifconfig $INTERFACE up
 iwconfig $INTERFACE channel $CHANNEL
 
 if [[ ! -f "$OUT_FILE" ]]; then
     touch "$OUT_FILE"
-    sqlite3 "$OUT_FILE" <<< "CREATE TABLE LOG (MAC_ADDR,IP_SRC,IP_DST,URL,TIMESTAMP,PROTOCOL);"
+    sqlite3 "$OUT_FILE" <<< "
+CREATE TABLE DNS (TIMESTAMP,TA,RA,IP_SRC,IP_DST,URL,DNS_RESPONSE,DNS_QRY_CLASS,DNS_QRY_TYPE);
+CREATE TABLE DHCP (TIMESTAMP,TA,RA,IP_SRC,IP_DST,IP_REQUEST,BOOTP_ID);"
 fi
 
 echo "Process PID : $$"
 eval $CAPTURE_PROGR $CAPTURE_FLAGS |
     eval $FILTER_PROGR "$FILTER_FLAGS" |
     tee /dev/fd/2 |
-    while IFS='|' read MAC_ADDR IP_SRC IP_DST BOOTP_ID URL TIMESTAMP PROTOCOL; do
-	if [[ -n "$BOOTP_ID" ]]; then
-	    echo "INSERT INTO LOG VALUES ('$MAC_ADDR','$IP_SRC','$IP_DST','$BOOTP_ID','$TIMESTAMP','$PROTOCOL');"
-	else
-	    echo "INSERT INTO LOG VALUES ('$MAC_ADDR','$IP_SRC','$IP_DST','$URL','$TIMESTAMP','$PROTOCOL');"
-	fi
+    while IFS='|' read TIMESTAMP TA RA IP_SRC IP_DST BOOTP_ID IP_REQUEST URL PROTOCOL DNS_RESPONSE DNS_QRY_CLASS DNS_QRY_TYPE; do
+        if [[ "$PROTOCOL" = "DNS" ]]; then
+            echo "INSERT INTO DNS VALUES ('$TIMESTAMP', '$TA', '$RA', '$IP_SRC','$IP_DST','$URL','$DNS_RESPONSE', '$DNS_QRY_CLASS', '$DNS_QRY_TYPE');"
+        else
+            echo "INSERT INTO DHCP VALUES ('$TIMESTAMP', '$TA', '$RA', '$IP_SRC','$IP_DST', '$IP_REQUEST', '$BOOTP_ID');"
+        fi
     done |
     sqlite3 "$OUT_FILE"
