e37752d514d03516fca2c30ec811dc231fae3b4b
C <thefiercerabbit@gmail.com>
2017-01-15 12:36:05 +0000

    removed DHCP capture, only get DNS request A type

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # dns-sniffer
-This is a very simple script sniffing DNS traffic of a local network.
+This is a very simple script sniffing DNS traffic of a local network. Only DNS request of type A are logged (one can easily extend to AAAA if needed).
 You will need *tshark* and a network adapter in *monitor mode*. If you are sniffing WiFi traffic, configure your adapter to listen to the right channel.
-DHCP packets are also logged, so you can check when some client has joined the network, which local IP it has been given, etc...
 
 # Why is it different ?
-*tshark* needs to read from a capture file. Even if you're not recording any event, but just displaying some stuff, *tshark* creates a temp file (generaly in */tmp*) which can get pretty big after some time...
-This script launches *dumpcap* on its own, and use a pipe redirection to avoid the creation of a temp file.
-
+*tshark* needs to read from a capture file. Even if you're not recording any event, but just displaying some stuff, *tshark* creates a temp file (generaly in */tmp*) which can get pretty big after some time... This script launches *dumpcap* on its own, and use a pipe redirection to avoid the creation of a temp file.
 This feature was compulsory for me, since it runs on a Raspberry Pi 2 with very few memory. Moreover, if some other hack would have been possible with the "ring buffer" option of *dumpcap*/*tshark*, it would still need to do IO operations on the disk (USB key in my case). Here, the redirected stdout fits in RAM.
 
--- dnsupdate.sh
+++ dnsupdate.sh
@@ -7,13 +7,12 @@ function _exit_function() {
 trap "_exit_function" 0
 
 INTERFACE="wlan0"
-OUT_FILE="/temp/dns.db"
+OUT_FILE="/home/pi/dns.db"
 OPTIONS="w:i:"
 
-SSID="MyRouter"
-PASSWORD="MyPassword"
-CHANNEL="6"
-RTR_MAC="01:23:45:67:89:AB"
+SSID="your_router_SSID"
+PASSWORD="your_router_password"
+CHANNEL="your_network_channel" 
 
 while getopts $OPTIONS opt; do
     case $opt in
@@ -24,9 +23,9 @@ while getopts $OPTIONS opt; do
 done
 
 CAPTURE_PROGR='dumpcap'
-CAPTURE_FLAGS="-q -i $INTERFACE -I -f '(link[0] != 0x80) and (wlan ra $RTR_MAC or wlan ta $RTR_MAC or wlan host $RTR_MAC)' -w -"
+CAPTURE_FLAGS="-q -i $INTERFACE -I -f '(link[0] != 0x80)' -w -"
 FILTER_PROGR='tshark'
-FILTER_FLAGS="-l -o wlan.enable_decryption:TRUE -o 'uat:80211_keys:\"wpa-pwd\",\"${PASSWORD}:${SSID}\"' -r - -Y '(dns) or (bootp.option.type==53)' -Tfields -E separator='|' -e frame.time_epoch -e wlan.ta -e wlan.ra -e ip.src -e ip.dst -e bootp.id -e bootp.option.requested_ip_address -e dns.qry.name -e _ws.col.Protocol -e dns.flags.response -e dns.qry.class -e dns.qry.type "
+FILTER_FLAGS="-l -o wlan.enable_decryption:TRUE -o 'uat:80211_keys:\"wpa-pwd\",\"${PASSWORD}:${SSID}\"' -r - -Y '(dns)' -Tfields -E separator='|' -e frame.time_epoch -e wlan.ta -e wlan.ra -e ip.src -e ip.dst -e _ws.col.Protocol -e dns.qry.name -e dns.flags.response -e dns.qry.type"
 
 if [ `whoami` != "root" ]
 then
@@ -42,23 +41,17 @@ iwconfig $INTERFACE channel $CHANNEL
 if [[ ! -f "$OUT_FILE" ]]; then
     touch "$OUT_FILE"
     sqlite3 "$OUT_FILE" <<< "
-CREATE TABLE DNS (TIMESTAMP real,TA,RA,IP_SRC,IP_DST,URL,DNS_RESPONSE int,DNS_QRY_CLASS,DNS_QRY_TYPE);
-CREATE TABLE DHCP(TIMESTAMP real,TA,RA,IP_SRC,IP_DST,IP_REQUEST,BOOTP_ID);"
+CREATE TABLE DNS (TIMESTAMP real,MAC_SRC,MAC_DST,IP_SRC,IP_DST,URL,DNS_RESPONSE int,DNS_QRY_TYPE);"
 fi
 
 echo "Process PID : $$"
+
 eval $CAPTURE_PROGR $CAPTURE_FLAGS |
     eval $FILTER_PROGR "$FILTER_FLAGS" |
     #tee /dev/fd/2 |
-    while IFS='|' read TIMESTAMP TA RA IP_SRC IP_DST BOOTP_ID IP_REQUEST URL PROTOCOL DNS_RESPONSE DNS_QRY_CLASS DNS_QRY_TYPE; do
-        if [[ "$PROTOCOL" = "DNS" ]]; then
-            echo "INSERT INTO DNS VALUES ('$TIMESTAMP', '$TA', '$RA', '$IP_SRC','$IP_DST','$URL','$DNS_RESPONSE', '$DNS_QRY_CLASS', '$DNS_QRY_TYPE');"
-	    printf "%10s %20s %20s %15s %15s %-5s %s\n" "$(date -d @${TIMESTAMP} '+%H:%M:%S')" "$TA" "$RA" "$IP_SRC" "$IP_DST" "$PROTOCOL" "$URL" >/dev/stderr
-        elif [[ "$PROTOCOL" = "DHCP" ]]; then
-            echo "INSERT INTO DHCP VALUES ('$TIMESTAMP', '$TA', '$RA', '$IP_SRC','$IP_DST', '$IP_REQUEST', '$BOOTP_ID');"
-	    printf "%10s %20s %20s %15s %15s %-5s %s\n" "$(date -d @${TIMESTAMP} '+%H:%M:%S')" "$TA" "$RA" "$IP_SRC" "$IP_DST" "$PROTOCOL" "$IP_REQUEST" >/dev/stderr
-        else
-	    printf "%10s %20s %20s %15s %15s %-5s\n" "$(date -d @${TIMESTAMP} "+%H:%M:%S")" "$TA" "$RA" "$IP_SRC" "$IP_DST" "$PROTOCOL"  >/dev/stderr
-	fi
+    while IFS='|' read TIMESTAMP MAC_SRC MAC_DST IP_SRC IP_DST PROTOCOL URL DNS_RESPONSE DNS_QRY_TYPE; do
+        if [[ "$PROTOCOL" = "DNS" && "$DNS_QRY_TYPE" = "1" ]]; then
+            echo "INSERT INTO DNS VALUES ('$TIMESTAMP', '$MAC_SRC', '$MAC_DST', '$IP_SRC','$IP_DST','$URL','$DNS_RESPONSE', '$DNS_QRY_TYPE');"
+	    printf "%10s %20s %20s %15s %15s %-3s %-s\n" "$(date -d @${TIMESTAMP} '+%H:%M:%S')" "$MAC_SRC" "$MAC_DST" "$IP_SRC" "$IP_DST" "$PROTOCOL" "$URL" >/dev/stderr
     done |
-    sqlite3 "$OUT_FILE"  
+    sqlite3 "$OUT_FILE"
