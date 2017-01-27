9038ad30ca8425bc387353281de766d2e54e1166
C <thefiercerabbit@gmail.com>
2017-01-27 23:24:07 +0000

    lock the database before adding content

--- dnsniff.sh
+++ dnsniff.sh
@@ -48,7 +48,7 @@ while ! [ -e "/sys/class/net/tap0" ]; do sleep 1; done # wait for the interface 
 tcpdump -tt -n -e -l -s 0 -i tap0 dst port 53 |
     gawk 'match($0,/^(\w+\.\w+) ([0-9a-f:]{17}) > ([0-9a-f:]{17}).* (\w+\.\w+\.\w+\.\w+)\.\w+ > (\w+\.\w+\.\w+\.\w+).*\? (.*)\. .*?$/,g) {
     printf "INSERT INTO DNS VALUES (\"" g[1]"\",\""g[2]"\",\""g[3]"\",\""g[4]"\",\""g[5]"\",\""g[6]"\");\n" > "/dev/stdout"; fflush("/dev/stdout");}' |
-    sqlite3 $DB_FILE
+    flock -x "$DB_FILE" -c "sqlite3 $DB_FILE"
 
 # add the following line to the awk command if you want a more complete log
 #    printf "%10s %17s %17s %15s %15s %-s\n",strftime("%D %T",g[1]),g[2],g[3],g[4],g[5],g[6] > "$LOG_FILE"; fflush("$LOG_FILE");
