8d1e8bc34d09d69d7b3b9c564f7e18b2e341dc7b
C <thefiercerabbit@gmail.com>
2017-01-28 01:52:12 +0000

    nice http status code check

--- dnsniff.sh
+++ dnsniff.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ENC="wpa" # or wep
-SSID="your_ssid"
-PWD="the_password"
-INTERFACE="monitored_interface"
-CHANNEL="11"
+ENC="wpa"
+SSID="to_replace"
+PWD="to_replace"
+INTERFACE="to_replace"
+CHANNEL="to_replace"
 
 DB_FILE="/home/pi/dns.db"
 LOG_FILE="/home/pi/dns.log" # can be replaced with '/dev/stderr' if running in foreground
@@ -31,7 +31,7 @@ ifconfig $INTERFACE up
 if [[ ! -f "$DB_FILE" ]]; then # create the database file if it does not exists
     touch "$DB_FILE"
     sqlite3 "$DB_FILE" <<< "
-CREATE TABLE DNS (TIMESTAMP real,MAC_SRC,MAC_DST,IP_SRC,IP_DST,URL);"
+CREATE TABLE DNS (TIMESTAMP real,MAC_SRC,MAC_DST,IP_SRC,IP_DST,URL,STATUS_CODE);"
 fi
 
 # Since we only write small amount of text, we consider the buffer of the system call 'write' to be large enough
@@ -47,7 +47,8 @@ while ! [ -e "/sys/class/net/tap0" ]; do sleep 1; done # wait for the interface 
 
 tcpdump -tt -n -e -l -s 0 -i tap0 dst port 53 |
     gawk 'match($0,/^(\w+\.\w+) ([0-9a-f:]{17}) > ([0-9a-f:]{17}).* (\w+\.\w+\.\w+\.\w+)\.\w+ > (\w+\.\w+\.\w+\.\w+).*\? (.*)\. .*?$/,g) {
-    printf "INSERT INTO DNS VALUES (\"" g[1]"\",\""g[2]"\",\""g[3]"\",\""g[4]"\",\""g[5]"\",\""g[6]"\");\n" > "/dev/stdout"; fflush("/dev/stdout");}' |
+    "curl -o /dev/null -m 5 --silent --head --write-out '%{http_code}' " g[6] | getline status_code;
+    printf "INSERT INTO DNS VALUES (\"" g[1]"\",\""g[2]"\",\""g[3]"\",\""g[4]"\",\""g[5]"\",\""g[6]"\",\""status_code"\");\n" > "/dev/stdout"; fflush("/dev/stdout");}' |
     flock -x "$DB_FILE" -c "sqlite3 $DB_FILE"
 
 # add the following line to the awk command if you want a more complete log
